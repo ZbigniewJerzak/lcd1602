ea781c1421661cbe38768e0c4c174097a564e4b6
Zbigniew Jerzak <ZbigniewJerzak@users.noreply.github.com>
2021-11-23 17:53:23 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    I2C_LCD1602.ShowString("Temperatur", 0, 0)
+    I2C_LCD1602.ShowNumber(input.temperature(), 0, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    I2C_LCD1602.clear()
+})
+I2C_LCD1602.LcdInit(0)
+I2C_LCD1602.on()
+I2C_LCD1602.clear()
 basic.forever(function () {
 	
 })
